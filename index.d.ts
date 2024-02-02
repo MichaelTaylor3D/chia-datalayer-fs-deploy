dd3db611e40ce1f1aa2e7a9c789d8acb0ad9e12d
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-02-02 17:02:34 -0500

    fix: make options optional

--- index.d.ts
+++ index.d.ts
@@ -2,20 +2,20 @@ import { EventEmitter } from "events";
 
 declare module "chia-datalayer-fs-deploy" {
   export interface Options {
-    datalayer_host: string;
-    wallet_host: string;
-    certificate_folder_path: string;
-    default_wallet_id: number;
-    default_fee: number;
-    default_mirror_coin_amount: number;
-    maximum_rpc_payload_size: number;
-    web2_gateway_port: number;
-    web2_gateway_host: string;
-    forceIp4Mirror: boolean;
-    mirror_url_override: string | null;
-    verbose: boolean;
-    num_files_processed_per_batch: number;
-    ignore_orphans: boolean;
+    datalayer_host?: string;
+    wallet_host?: string;
+    certificate_folder_path?: string;
+    default_wallet_id?: number;
+    default_fee?: number;
+    default_mirror_coin_amount?: number;
+    maximum_rpc_payload_size?: number;
+    web2_gateway_port?: number;
+    web2_gateway_host?: string;
+    forceIp4Mirror?: boolean;
+    mirror_url_override?: string | null;
+    verbose?: boolean;
+    num_files_processed_per_batch?: number;
+    ignore_orphans?: boolean;
   }
 
   export interface OperationEmitter {
